eb7453b943b2a50958d7d96712971b040208f061
sam_ata <samjohnsonn23w@gmail.com>
2021-03-16 18:21:58 +0300

    :c

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-require('dotenv').config()
-const client = new (require('./src/classes/App'))()
-const Logger = require('./src/util/Logger')
+require('dotenv').config();
+const client = new (require('./src/classes/App'));
+const Logger = require('./src/util/Logger');
 
-;(async function () {
-    await client.registerRoutes()
+(async function () {
+    await client.registerRoutes();
     await client.listen(() => {
-        Logger.info(`Express serving on port ${client.port}`)
-    }, true)
-})()
+        Logger.info(`Express serving on port ${client}`);
+    }, true);
+})();
